7194e841a743a57412efd5195875098d5aa431eb
Collie Tsai <qetup1988+github@gmail.com>
2022-08-25 16:12:44 +0800

    :white_check_mark: ac 383

---
+++ 383.cpp
@@ -0,0 +1,27 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> rans, mag;
+	for(const char& ch : magazine){
+	    if(mag.find(ch) != mag.end())
+		mag[ch] ++;
+	    else
+		mag[ch] = 1;
+	}
+	
+	for(const char& ch: ransomNote){
+	    if(mag.find(ch) == mag.end())
+		return false;
+	    else{
+		mag[ch]--;
+		if(mag[ch] == 0)
+		    mag.erase(ch);
+	    }
+	}
+	return true;
+    }
+};
+
