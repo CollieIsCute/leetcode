0b3f190f3858c2744f1e00ddd8174748958d1804
Collie Tsai <qetup1988+github@gmail.com>
2022-09-29 04:23:49 +0800

    ac 1114

---
+++ 1114.cpp
@@ -0,0 +1,26 @@
+#include <functional>
+#include <atomic>
+using namespace std;
+
+class Foo {
+public:
+    atomic<int> cnt = 0;
+    void first(function<void()> printFirst) {
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+		cnt++;
+    }
+
+    void second(function<void()> printSecond) {
+        while(cnt!=1);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+		cnt++;
+    }
+
+    void third(function<void()> printThird) {
+		while(cnt!=2);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
