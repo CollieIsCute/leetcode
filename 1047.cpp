8408a16b696e75f5c74a1de80a69f69ae48a68ea
Collie Tsai <qetup1988+github@gmail.com>
2022-11-10 23:46:04 +0800

    ac 1047

---
+++ 1047.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include<utility>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string tmp_stack;
+        while(!s.empty()){
+            if(tmp_stack.empty() || tmp_stack.back() != s.back()){
+				tmp_stack.push_back(move(s.back()));
+				s.pop_back();
+			}
+            else {
+				tmp_stack.pop_back();
+				s.pop_back();
+			}
+		}
+        while(!tmp_stack.empty()){
+            s.push_back(move(tmp_stack.back()));
+			tmp_stack.pop_back();
+		}
+		return s;
+	}
+};
