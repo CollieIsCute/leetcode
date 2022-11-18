e1e0fd3f532e562fd2d724b644854a2291ec459d
Collie Tsai <qetup1988+github@gmail.com>
2022-11-18 15:31:34 +0800

    263

---
+++ 263.cpp
@@ -0,0 +1,14 @@
+void remove_factor(int& target, int factor){
+	for(;target && target == (target/factor)*factor; target/=factor)
+		;
+}
+
+class Solution {
+public:
+    bool isUgly(int n) {
+		remove_factor(n, 2);
+		remove_factor(n, 3);
+		remove_factor(n, 5);
+		return n == 1;
+	}
+};
