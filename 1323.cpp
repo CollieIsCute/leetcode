e671b289e93846e273cc6ee601e410b0b83e4b71
Collie Tsai <qetup1988+github@gmail.com>
2022-11-19 17:58:46 +0800

    1323

---
+++ 1323.cpp
@@ -0,0 +1,18 @@
+#include <sstream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int maximum69Number (int num) {
+		stringstream buf;
+		buf << num;
+		string checker = buf.str();
+		for(auto& ch : checker)
+			if(ch == '6'){
+				ch = '9';
+				break;
+			}
+		return stoi(checker);
+	}
+};
