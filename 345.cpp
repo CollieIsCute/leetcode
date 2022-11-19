ca200a208a9e21ec4b34ee9e235555ea50218d28
Collie Tsai <qetup1988+github@gmail.com>
2022-11-19 14:14:58 +0800

    345

---
+++ 345.cpp
@@ -0,0 +1,26 @@
+#include <set>
+#include <string>
+using namespace std;
+
+set<char> vowels = { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+
+class Solution {
+public:
+	string reverseVowels(string s) {
+		for(int hi = s.size() - 1, lo = 0; lo < hi;){
+			auto find_vowel_pair = [&]() -> void {
+				while((vowels.find(s[lo]) == vowels.end()) && lo<hi)
+					++lo;
+				while((vowels.find(s[hi]) == vowels.end()) && lo < hi)
+					--hi;
+			};
+			find_vowel_pair();
+			if(lo < hi) {
+				swap(s[lo], s[hi]);
+				++lo;
+				--hi;
+			}
+		}
+		return s;
+	}
+};
