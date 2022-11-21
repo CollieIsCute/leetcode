a5927c398e1a947e6cfc3c45742dfe7711684542
Collie Tsai <qetup1988+github@gmail.com>
2022-11-22 02:34:34 +0800

    9

---
+++ 9.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		vector<int> v;
+		if(x<0)
+			return false;
+		while(x) {
+			v.emplace_back(move(x % 10));
+			x /= 10;
+		}
+		for(int i = 0, j = v.size() - 1; i < j; i++, j--)
+			if(v[i] != v[j])
+				return false;
+		return true;
+	}
+};
