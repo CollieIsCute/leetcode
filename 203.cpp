14faf40b2be547064c830c2611341e1cd53b97e8
Collie Tsai <qetup1988+github@gmail.com>
2022-11-26 01:42:23 +0800

    203

---
+++ 203.cpp
@@ -0,0 +1,23 @@
+
+// Definition for singly-linked list.
+// struct ListNode {
+// 	int val;
+// 	ListNode* next;
+// 	ListNode() : val(0), next(nullptr) {}
+// 	ListNode(int x) : val(x), next(nullptr) {}
+// 	ListNode(int x, ListNode* next) : val(x), next(next) {}
+// };
+
+class Solution {
+public:
+	ListNode* removeElements(ListNode* head, int val) {
+		ListNode** indirect = &head;
+		for(; *indirect;) {
+			if((*indirect)->val == val)
+				*indirect = (*indirect)->next;
+			else
+				indirect = *indirect ? (&(*indirect)->next) : indirect;
+		}
+		return head;
+	}
+};
