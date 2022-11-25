038b33bf0c0f4ab1887d8c3f05f19ca8d3368741
Collie Tsai <qetup1988+github@gmail.com>
2022-11-26 03:22:04 +0800

    142

---
+++ 142.cpp
@@ -0,0 +1,27 @@
+
+// Definition for singly-linked list.
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(NULL) {}
+// };
+
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+	ListNode* detectCycle(ListNode* head) {
+		set<ListNode*> record;
+		ListNode* beg = [&]() {
+			for(ListNode* cur = head; cur; cur = cur->next) {
+				if(record.find(cur) != record.end())
+					return cur;
+				else
+					record.insert(cur);
+			}
+			return static_cast<ListNode*>(nullptr);
+		}();
+		return beg;
+	}
+};
