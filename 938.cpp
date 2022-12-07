5dad3e11bcfc2ab575b4ff083c70366403ea6efc
Collie Tsai <qetup1988+github@gmail.com>
2022-12-07 20:39:17 +0800

    938

---
+++ 938.cpp
@@ -0,0 +1,30 @@
+
+// Definition for a binary tree node.
+// struct TreeNode {
+// 	int val;
+// 	TreeNode* left;
+// 	TreeNode* right;
+// 	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// 	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// 	TreeNode(int x, TreeNode* left, TreeNode* right)
+// 		: val(x), left(left), right(right) {}
+// };
+
+bool is_in_range(TreeNode* target, int low, int high){
+	if(low <= target->val && target->val <= high)
+	cout << target->val << " is in range\n";
+	return low <= target->val && target->val <= high;
+}
+
+class Solution {
+public:
+	int rangeSumBST(TreeNode* root, int low, int high) {
+		int ret_val = 0;
+		if(!root)
+			return ret_val;
+		if(is_in_range(root, low, high))
+			ret_val = root->val;
+		return ret_val + rangeSumBST(root->left, low, high)
+			   + rangeSumBST(root->right, low, high);
+	}
+};
