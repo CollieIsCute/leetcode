41f751e65414d3b4587e5f0b21506a23e9a66997
Collie Tsai <qetup1988+github@gmail.com>
2022-12-12 22:58:28 +0800

    70

---
+++ 110.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+	int get_height(TreeNode* root) {
+		if(!root)
+			return 0;
+		return max(get_height(root->left), get_height(root->right)) + 1;
+	}
+
+	bool isBalanced(TreeNode* root) {
+		if(!root)
+			return true;
+		int left = get_height(root->left);
+		int right = get_height(root->right);
+		if(abs(left - right) > 1)
+			return false;
+		if(isBalanced(root->left) && isBalanced(root->right))
+			return true;
+		return false;
+	}
+};
---
+++ 70.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+	int fab[46];
+
+public:
+	Solution(){
+		fab[0] = fab[1] = 1;
+		for(int i = 2; i <= 45; i++)
+			fab[i] = fab[i - 1] + fab[i - 2];
+	}
+	int climbStairs(int n) {
+		return fab[n];
+	}
+};
