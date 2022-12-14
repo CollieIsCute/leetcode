001c91a6069f02dad33fe9bed879ef2a260a8d22
Collie Tsai <qetup1988+github@gmail.com>
2022-12-14 18:48:21 +0800

    198

---
+++ 198.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int rob(const vector<int>& nums) {
+		vector<int> ans(nums);
+		for(int i = 1; i < ans.size(); ++i)
+			if(i == 1)
+				ans[i] = max(ans[i], ans[i - 1]);
+			else
+				ans[i] = max(ans[i - 1], ans[i - 2] + ans[i]);
+		return ans.back();
+	}
+};
