b52056bc44f5ee1f1b0baf53c221ffd27989daa1
Collie Tsai <qetup1988+github@gmail.com>
2022-12-16 17:10:23 +0800

    232

---
+++ 232.cpp
@@ -0,0 +1,44 @@
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+
+#include <vector>
+using namespace std;
+
+class MyQueue {
+	vector<int> stk, stk_inv;
+
+public:
+	MyQueue() {}
+
+	void push(int x) {
+		stk.push_back(x);
+	}
+
+	int pop() {
+		peek();
+		int ret = stk_inv.back();
+		stk_inv.pop_back();
+		return ret;
+	}
+
+	int peek() {
+		if(stk_inv.empty()) {
+			while(!stk.empty()) {
+				stk_inv.push_back(stk.back());
+				stk.pop_back();
+			}
+			stk.clear();
+		}
+		return stk_inv.back();
+	}
+
+	bool empty() {
+		return !(stk.size() + stk_inv.size());
+	}
+};
