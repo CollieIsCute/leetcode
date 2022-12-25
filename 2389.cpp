2c57372db997dbe125cf726e14da18099688e5da
Collie Tsai <qetup1988+github@gmail.com>
2022-12-26 04:18:09 +0800

    2389

---
+++ 2389.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+int find_index(const vector<int>& v, const int target) {
+	return upper_bound(v.begin(), v.end(), target) - v.begin();
+}
+
+class Solution {
+public:
+	vector<int> answerQueries(vector<int>& nums, const vector<int>& queries) {
+		sort(nums.begin(), nums.end(), [](int a, int b) { return a < b; });
+		vector<int> dp(nums.size()), ans(queries.size());
+		partial_sum(nums.begin(), nums.end(), dp.begin());
+		for(int i = 0; i < ans.size(); i++)
+			ans[i] = find_index(dp, queries[i]);
+		return ans;
+	}
+};
