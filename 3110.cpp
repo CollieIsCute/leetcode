bea4965d2e397a6cf3fb3327cd5daa5a3658e7d4
牧羊犬真Q <colliemst@gmail.com>
2024-06-01 12:29:47 +0800

    ac 3110

---
+++ 3110.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        auto cnt = 0;
+        for (auto i = 0; i < s.size() - 1; i++) {
+          cnt += abs(s[i] - s[i+1]);
+        }
+        return cnt;
+    }
+};
