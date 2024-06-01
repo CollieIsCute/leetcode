af6e1306c02fd06b81efa3c3dfff371704510556
牧羊犬真Q <colliemst@gmail.com>
2024-06-01 14:53:18 +0800

    test format

--- 69.cpp
+++ 69.cpp
@@ -1,14 +1,15 @@
 class Solution {
- public:
-  int mySqrt(const long long x) {
-    long long sqrt = x >> 1;
-    while (sqrt * sqrt > x) {
-      sqrt = sqrt >> 1;
+public:
+    int mySqrt(const long long x) {
+        long long sqrt = x >> 1;
+        while (sqrt * sqrt > x) {
+            sqrt = sqrt >> 1;
+        }
+        if (sqrt * sqrt == x)
+            return sqrt;
+        while ((sqrt + 1) * (sqrt + 1) <= x) {
+            ++sqrt;
+        }
+        return sqrt;
     }
-    if (sqrt * sqrt == x) return sqrt;
-    while ((sqrt + 1) * (sqrt + 1) <= x) {
-      ++sqrt;
-    }
-    return sqrt;
-  }
 };
