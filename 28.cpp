fe924d920942e99567ee68e2e81e4caed65e4a2b
牧羊犬真Q <colliemst@gmail.com>
2024-06-01 12:36:56 +0800

    ac 28

---
+++ 28.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string_view haystack, string_view needle) {
+        auto find = haystack.find(needle);
+        if (find == string_view::npos)
+            return -1;
+        return find;
+    }
+};
