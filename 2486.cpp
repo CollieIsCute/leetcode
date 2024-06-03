11017b1cdff9ea3709295a505bbe39c98fa0e11a
牧羊犬真Q <colliemst@gmail.com>
2024-06-03 08:52:10 +0800

    Create 2486.cpp

---
+++ 2486.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int appendCharacters(string_view s, string_view t) {
+        for (const auto& c : s) {
+            if (c == t.front()) {
+                t.remove_prefix(1);
+            }
+        }
+        return t.size();
+    }
+};
