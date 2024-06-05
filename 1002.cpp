2d8127376a59bbdcf15c512899d859bedb928b72
牧羊犬真Q <colliemst@gmail.com>
2024-06-05 21:34:16 +0800

    ac 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<char> inter(words[0].begin(), words[0].end());
+        ranges::sort(inter);
+        for(const auto& w: words){
+            vector<char> tmp;
+            vector<char> cur(w.begin(), w.end());
+            ranges::sort(cur);
+            ranges::set_intersection(inter, cur, back_inserter(tmp));
+            inter = std::move(tmp);
+        }
+        vector<string> ret;
+        for(const auto ch: inter){
+            ret.push_back({ch});
+        }
+        return ret;
+    }
+};
