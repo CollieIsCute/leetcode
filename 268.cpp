40b647e389dc7c56c969ac7b3bed2e6eae4a2c2c
牧羊犬真Q <colliemst@gmail.com>
2024-06-05 22:11:55 +0800

    ac 268.cpp

---
+++ 268.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        const auto NumsSize = nums.size();
+        nums.resize(NumsSize+1, -1);
+        for(auto i = 0; i < NumsSize; i++){
+            while(nums[i] != i && nums[i]!= -1){
+                std::swap(nums[i], nums[nums[i]]);
+            }
+        }
+        for(auto i = 0; i< nums.size(); i++){
+            if(nums[i] != i) return i;
+        }
+        return nums.size();
+    }
+};
