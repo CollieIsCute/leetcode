170b709d26cfbce0a4bcea0e6cc7af942f609e2b
牧羊犬真Q <colliemst@gmail.com>
2024-06-21 02:50:05 +0800

    Create 2427.cpp

---
+++ 2427.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        auto GCD = std::gcd(a, b);
+        auto cnt = 0;
+        for (auto i = 1; i <= GCD; i++) {
+            if (GCD % i)
+                continue;
+            ++cnt;
+        }
+        return cnt;
+    }
+};
