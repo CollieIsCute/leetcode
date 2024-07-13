e7cea8c1ac7a6f7cb871c09767c4c44e4b161bee
牧羊犬真Q <colliemst@gmail.com>
2024-07-13 14:54:15 +0800

    Create 1598.cpp

---
+++ 1598.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        auto cnt = 0;
+        for (const auto& log : logs) {
+            if (log == "../") {
+                if (cnt > 0)
+                    --cnt;
+            } else if (log == "./") {
+            } else {
+                ++cnt;
+            }
+        }
+        return cnt;
+    }
+};
